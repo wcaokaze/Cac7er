447036729d0fa5510ab7888a19b4eeccc3d5a63d
wcaokaze <wcaokaze@gmail.com>
2018-08-14 00:43:49 +0900

    added Date serializer

---
+++ src/main/java/cac7er/serializer/util/date.kt
@@ -0,0 +1,15 @@
+package cac7er.serializer.util
+
+import cac7er.*
+import cac7er.serializer.*
+import java.util.Date
+
+fun CacheOutput.writeDate(value: Date) {
+   val time = value.time
+   writeLong(time)
+}
+
+fun CacheInput.readDate(): Date {
+   val time = readLong()
+   return Date(time)
+}
