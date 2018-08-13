32593502426145739cd6d6283d780c33c98410dd
wcaokaze <wcaokaze@gmail.com>
2018-08-13 23:44:11 +0900

    added BooleanArray deserializer
    and simplified. Using BufferedIOStream, it has no problem.

--- src/main/java/cac7er/serializer/array.kt
+++ src/main/java/cac7er/serializer/array.kt
@@ -5,25 +5,15 @@ import cac7er.*
 private const val BUFFER_SIZE = 256
 
 fun CacheOutput.writeBooleanArray(value: BooleanArray) {
-   val size = value.size
+   writeInt(value.size)
 
-   val b = ByteArray(BUFFER_SIZE)
-
-   writeInt(size)
-
-   for (offset in 0 until size step BUFFER_SIZE) {
-      if (offset + BUFFER_SIZE < size) {
-         for (i in 0 until BUFFER_SIZE) {
-            b[i] = if (value[offset + i]) 1 else 0
-         }
-
-         stream.write(b)
-      } else {
-         for (i in offset until size) {
-            b[i] = if (value[i]) 1 else 0
-         }
-
-         stream.write(b, 0, size - offset)
-      }
+   for (b in value) {
+      writeBoolean(b)
    }
+}
+
+fun CacheInput.readBooleanArray(): BooleanArray {
+   val size = readInt()
+
+   return BooleanArray(size) { readBoolean() }
 }
