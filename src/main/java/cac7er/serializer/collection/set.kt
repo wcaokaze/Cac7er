a13114482a2b804c6181a2f9cec0cbcde8d2c51c
wcaokaze <wcaokaze@gmail.com>
2018-08-14 00:37:10 +0900

    moved package

---
+++ src/main/java/cac7er/serializer/collection/set.kt
@@ -0,0 +1,31 @@
+package cac7er.serializer.collection
+
+import cac7er.*
+import cac7er.serializer.*
+
+inline fun <T> CacheOutput.writeSet
+      (value: Set<T>, elementSerializer: Serializer<T>)
+{
+   writeInt(value.size)
+
+   for (t in value) {
+      elementSerializer(t)
+   }
+}
+
+inline fun <T> CacheInput.readSet(elementDeserializer: Deserializer<T>): Set<T> {
+   return readMutableSet(elementDeserializer)
+}
+
+inline fun <T> CacheInput.readMutableSet
+      (elementDeserializer: Deserializer<T>): MutableSet<T>
+{
+   val size = readInt()
+   val set = HashSet<T>()
+
+   for (i in 0 until size) {
+      set += elementDeserializer()
+   }
+
+   return set
+}
