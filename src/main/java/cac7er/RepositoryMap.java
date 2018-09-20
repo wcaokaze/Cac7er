458111991fc48cab375de9e9fc8cb60db2a2f80e
wcaokaze <wcaokaze@gmail.com>
2018-09-21 01:26:51 +0900

    changed it as Java

---
+++ src/main/java/cac7er/RepositoryMap.java
@@ -0,0 +1,7 @@
+package cac7er;
+
+final class RepositoryMap {
+   final Repository<?, ?> get(final String name) {
+
+   }
+}
--- src/main/java/cac7er/RepositoryMap.kt
+++
@@ -1,7 +0,0 @@
-package cac7er
-
-internal class RepositoryMap {
-   operator fun get(name: String): Repository<*, *> {
-      TODO()
-   }
-}
