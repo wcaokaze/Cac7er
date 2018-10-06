5f9e00a6a42843510825636adcad9076e0cb853e
wcaokaze <wcaokaze@gmail.com>
2018-10-06 23:12:51 +0900

    added UrlSerializerTest

---
+++ src/test/java/cac7er/serealizer/net/UrlSerializerTest.kt
@@ -0,0 +1,53 @@
+package cac7er.serealizer.net
+
+import cac7er.*
+
+import org.junit.*
+import org.junit.runner.*
+import org.junit.runners.*
+
+import java.io.*
+import cac7er.serializer.*
+import cac7er.serializer.net.*
+
+import java.net.URL
+
+@RunWith(JUnit4::class)
+class UrlSerializerTest {
+   private val testDir = File("testOutput")
+
+   @After fun deleteDir() {
+      testDir.deleteRecursively()
+   }
+
+   @Test fun url() {
+      val url1 = URL("http://2wiqua.wcaokaze.com/")
+
+      val url2 = URL("https://play.google.com/store/apps/details?" +
+            "id=com.wcaokaze.android.twiqua")
+
+      val cac7er = Cac7er("url", testDir) {}
+
+      with (cac7er.getCacheOutput("url")) {
+         writeUrl(url1)
+         writeUrl(url2)
+      }
+
+      with (cac7er.getCacheInput("url")) {
+         assert(readUrl() == url1)
+         assert(readUrl() == url2)
+      }
+   }
+
+   private fun Cac7er.getCacheOutput(name: String): CacheOutput {
+      val file = File(testDir, name)
+
+      return CacheOutput(RandomAccessFile(file, "rw"), file, this)
+   }
+
+   private fun Cac7er.getCacheInput(name: String): CacheInput {
+      val file = File(testDir, name)
+
+      return CacheInput(RandomAccessFile(file, "r"), this)
+   }
+}
