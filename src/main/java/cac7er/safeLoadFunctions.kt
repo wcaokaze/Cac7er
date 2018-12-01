2c57e40e1eb14fbc5d680d999b18f2f1e5892f7a
wcaokaze <wcaokaze@gmail.com>
2018-12-01 23:21:22 +0900

    added safe load functions

--- src/main/java/cac7er/safeLoadFunctions.kt
+++ src/main/java/cac7er/safeLoadFunctions.kt
@@ -1,6 +1,6 @@
 package cac7er
 
-import java.io.*
+import kotlinx.coroutines.*
 
 suspend fun <K, V> Repository<K, V>.loadOrNull(key: K): Cache<V>? {
    return try {
@@ -18,6 +18,24 @@ suspend fun <K, V> WritableRepository<K, V>.loadOrNull(key: K): WritableCache<V>
    }
 }
 
+suspend fun <K, V> Repository<K, V>.loadOrCancel(key: K): Cache<V> {
+   return try {
+      load(key)
+   } catch (e: Exception) {
+      throw CancellationException()
+   }
+}
+
+suspend fun <K, V> WritableRepository<K, V>.loadOrCancel(key: K): WritableCache<V> {
+   return try {
+      load(key)
+   } catch (e: Exception) {
+      throw CancellationException()
+   }
+}
+
+// =============================================================================
+
 suspend fun <K, V> Repository<K, V>.loadWeakCacheOrNull(key: K): WeakCache<V>? {
    return try {
       loadWeakCache(key)
@@ -31,15 +49,43 @@ suspend fun <K, V> WritableRepository<K, V>
 {
    return try {
       loadWeakCache(key)
-   } catch (e: IOException) {
+   } catch (e: Exception) {
       null
    }
 }
 
+suspend fun <K, V> Repository<K, V>.loadWeakCacheOrCancel(key: K): WeakCache<V> {
+   return try {
+      loadWeakCache(key)
+   } catch (e: Exception) {
+      throw CancellationException()
+   }
+}
+
+suspend fun <K, V> WritableRepository<K, V>
+      .loadWeakCacheOrCancel(key: K): WritableWeakCache<V>
+{
+   return try {
+      loadWeakCache(key)
+   } catch (e: Exception) {
+      throw CancellationException()
+   }
+}
+
+// =============================================================================
+
 suspend fun <T> LazyCache<T>.getOrNull(accessCount: Float = .0f): T? {
    return try {
       get(accessCount)
-   } catch (e: IOException) {
+   } catch (e: Exception) {
       null
+   }
+}
+
+suspend fun <T> LazyCache<T>.getOrCancel(accessCount: Float = .0f): T {
+   return try {
+      get(accessCount)
+   } catch (e: Exception) {
+      throw CancellationException()
    }
 }
